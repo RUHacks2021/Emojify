96ac2c7543f8d754fdc01b72fbec4c06121732ae
Matt Belanger <mattmbelanger@hotmail.com>
2021-05-01 19:43:57 -0400

    fixed img breaking bug

--- find.js
+++ find.js
@@ -135,8 +135,21 @@ function applyEmojification(){
 
     function emojiReplace(value,key,map) {   //called by foreach
         const text = this.querySelectorAll('u,b,dd,h1,h2,h3,h4,h5,p,li,td,caption,span,a'); //all text nodes
+        let flag = 0
+        const img = this.querySelectorAll('img,g-img'); //all img nodes
         for(let i = 0; i<text.length; i++){
-            console.log(text[i].innerHTML)
+
+            flag = 0
+            for(let j = 0; j < img.length;j++){ //prevents the extension from damaging image links
+                if(text[i].contains(img[j])){
+                    flag = 1
+                    break
+                }
+            }
+            if(flag===1) {
+                continue
+            }
+
             text[i].innerHTML = text[i].innerHTML.replace(key,value)
             text[i].innerHTML = text[i].innerHTML.replace(key.toUpperCase(),value)
             text[i].innerHTML = text[i].innerHTML.replace(key[0].toUpperCase()+key.substr(1),value)
